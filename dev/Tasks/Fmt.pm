b91af70ecbbbbdd5474dab70884061f4da0aee3f
12ball <pub@janie.page>
2023-10-01 21:03:28 -0500

    1696212208

--- dev/Tasks/Fmt.pm
+++ dev/Tasks/Fmt.pm
@@ -1,4 +1,4 @@
-use 5.28.0;
+use 5.38.0;
 use strict;
 use warnings;
 use FindBin;
@@ -12,13 +12,14 @@ our @EXPORT = qw(fmt);
 
 sub fmt {
     my $fmt_cmd = sub {
-        if (   ( $File::Find::dir ne $File::Find::name )
-            && ( $File::Find::dir !~ /^local\// )
-            && ( index( $File::Find::dir, "/node_modules" ) == -1 )
+        if (   ( -f $_ )
+            && ( index( $_, "/local/" ) != -1 )
+            && ( index( $_, "/node_modules/" ) != -1 )
             && ( $_ =~ /\.(md|js|jsx|json|ts|tsx)$/ ) )
         {
-            say $File::find::dir;
-            system( ( "deno", "fmt", $File::Find::name ) );
+            say $File::Find::dir;
+
+            # system( ( "deno", "fmt", $File::Find::name ) );
         }
     };
     File::Find::find( $fmt_cmd, ( $FindBin::Bin . "/.." ) );
--- dev/Tasks/Readme.pm
+++ dev/Tasks/Readme.pm
@@ -1,4 +1,4 @@
-use 5.28.0;
+use 5.38.0;
 use strict;
 use warnings;
 use FindBin;
