d1dbf2ca375db9931a7b8e9aea9ba74368fe7e00
12ball <pub@janie.page>
2023-10-04 20:38:49 -0500

    1696469929

--- cpanfile
+++ cpanfile
@@ -1,5 +1,2 @@
 requires 'DBD::SQLite';
-requires 'IO::Async';
-requires 'HTTP::Server::Simple';
-requires 'HTTP::Response';
-requires 'Net::Server';
+requires 'Mojolicious';
--- lib/Server.pm
+++ lib/Server.pm
@@ -3,27 +3,6 @@ use warnings;
 use 5.38.0;
 use FindBin;
 use lib "$FindBin::Bin/../local/lib/perl5";
-use HTTP::Server::Simple::CGI;
-use HTTP::Response;
 
-package Server;
-use base qw(HTTP::Server::Simple::CGI);
+use Mojo::Base 'Mojolicious';
 
-my %dispatch = ();
-
-sub net_server { 'Net::Server'; }
-
-sub handle_request {
-    my ( $self, $cgi ) = @_;
-
-    my $path    = $cgi->path_info();
-    my $handler = $dispatch{$path};
-
-    if (1) {
-        print HTTP::Response->new( 200, "joemoe" )->decoded_content;
-        $handler->($cgi);
-
-    }
-}
-
-1;
