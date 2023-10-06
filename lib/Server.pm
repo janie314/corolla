80e0e1698f3d7feb492f104d2122a96ef23d7ff4
12ball <pub@janie.page>
2023-10-05 21:25:08 -0500

    1696559107

--- lib/Server.pm
+++ lib/Server.pm
@@ -3,14 +3,17 @@ use warnings;
 use 5.38.0;
 use FindBin;
 use lib "$FindBin::Bin/../local/lib/perl5";
-
+use Carp;
 use Mojolicious::Lite;
 
 sub run_server {
-    get '/' => sub {
+    my ( $port, $root ) = @_;
+    $root =~ s~^([^/])~/$1~;
+    $root =~ s~/$~~;
+    get $root. '/' => sub {
         my ($c) = @_;
         $c->render( text => 'Hello World!' );
     };
     app->secrets( ['I suppose this is important.'] );
-    app->start( 'daemon', '-l', 'http://*:12345' );
+    app->start( 'daemon', '-l', "http://*:$port" );
 }
