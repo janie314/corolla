d2e590b38a384dbb957adaff4e4b2ba6df386e1d
12ball <pub@janie.page>
2023-10-05 21:04:08 -0500

    1696557848

--- lib/Server.pm
+++ lib/Server.pm
@@ -4,5 +4,13 @@ use 5.38.0;
 use FindBin;
 use lib "$FindBin::Bin/../local/lib/perl5";
 
-use Mojo::Base 'Mojolicious';
+use Mojolicious::Lite;
 
+sub run_server {
+    get '/' => sub {
+        my ($c) = @_;
+        $c->render( text => 'Hello World!' );
+    };
+
+    app->start( 'daemon', '-l', 'http://*:12345' );
+}
