f468712b524c580d3a8178656337028b4f6b8d58
12ball <pub@janie.page>
2023-10-07 10:48:43 -0500

    Set up some more endpoints

--- lib/Server.pm
+++ lib/Server.pm
@@ -5,15 +5,38 @@ use FindBin;
 use lib "$FindBin::Bin/../local/lib/perl5";
 use Carp;
 use Mojolicious::Lite;
+use Future::Mutex;
+
+sub open_db_conn {
+    my ($dbfile) = @_;
+    $dbfile =~ s~^:~./:~;
+    my $db   = DBI->connect("dbi:SQLite:dbname=$dbfile") or die $DBI::errstr;
+    my $lock = Futur::Mutex->new;
+}
 
 sub run_server {
-    my ( $port, $root ) = @_;
+
+    # process method arguments
+    my ( $port, $root, $dbpath ) = @_;
     $root =~ s~^([^/])~/$1~;
     $root =~ s~/$~~;
-    get $root. '/' => sub {
+
+    # set up endpoints
+    get $root. '/read' => sub {
         my ($c) = @_;
         $c->render( text => 'Hello World!' );
     };
+    set $root . '/write' => sub {
+        my ($c) = @_;
+    };
     app->secrets( ['I suppose this is important.'] );
+
+    # database connection
+    helper db => sub { state $db = open_db_conn $dbpath; };
+
+    # init DB
+    app->db->do("create table if not exists t (x int);");
+
+    # main execution
     app->start( 'daemon', '-l', "http://*:$port" );
 }
