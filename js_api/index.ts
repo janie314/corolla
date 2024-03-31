3e95894027922d81c73f7865b2d857d2b019e169
12ball <pub@janie.page>
2024-03-30 22:13:52 -0500

    1711854832

--- js_api/index.ts
+++ js_api/index.ts
@@ -29,42 +29,45 @@ class API {
    * @param args Key-value map of query arguments.
    * @returns The query's SQL results.
    */
-  public async read_query(
-    query: string,
-    args?: Args,
-  ): Promise<Result[]> {
-    const url_query_args = args === undefined
-      ? ""
-      : "?" + new URLSearchParams(args);
-    const res: string[][] = await fetch(
-      `${this.url_base}/read/${query}${url_query_args}`,
-    )
-      .then((
-        r,
-      ) => r.json());
-    if (res.length === 0) {
-      return [];
-    }
-    const headers = res[0];
-    return res.slice(1).map((row): Result => {
-      return Object.fromEntries(
-        headers.map((key: string, i: number) => [key, row[i]]),
-      ) as Result;
-    });
+  public make_read_query<Result>(query: string) {
+    return async (
+      args?: Args,
+    ): Promise<Result[]> => {
+      const url_query_args = args === undefined
+        ? ""
+        : "?" + new URLSearchParams(args);
+      const res: string[][] = await fetch(
+        `${this.url_base}/read/${query}${url_query_args}`,
+      )
+        .then((
+          r,
+        ) => r.json());
+      if (res.length === 0) {
+        return [];
+      }
+      const headers = res[0];
+      return res.slice(1).map((row): Result => {
+        return Object.fromEntries(
+          headers.map((key: string, i: number) => [key, row[i]]),
+        ) as Result;
+      });
+    };
   }
 
   /**
    * @param query The name of the Corolla query query.
    * @param args Key-value map of query arguments.
    */
-  public async write_query(query: string, args: Args) {
-    return await fetch(`${this.url_base}/write/${query}`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(args),
-    });
+  public async make_write_query(query: string) {
+    return async (args: Args) => {
+      return await fetch(`${this.url_base}/write/${query}`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(args),
+      });
+    };
   }
 }
 
