0419a67e0f08b4bd896c5f5ca2a7166ffa0d14d0
12ball <pub@janie.page>
2024-03-31 21:17:32 -0500

    1711937852

--- js_api/api_test.ts
+++ js_api/api_test.ts
@@ -1,18 +1,17 @@
-import { expect, test } from "bun:test";
 import { Corolla } from "../js_api/index";
+import { expect, test } from "bun:test";
 
-const corolla = new Corolla("/test");
+const corolla = new Corolla("http://127.0.0.1:50000", "/test");
 
 const write01 = corolla.make_write_query<
   { c: string }
->("read01");
+>("write01");
 await write01({ c: new Date().toISOString() });
 
 const read01 = corolla.make_read_query<
   {},
   { id: string; c: string }
 >("read01");
-const res = await read01({});
 
 test("write query", async () => {
   let res = await write01({ c: "carrot" });
@@ -20,6 +19,13 @@ test("write query", async () => {
   res = await write01({ c: "tomato" });
   expect(res.status).toBe(200);
   res = await write01({ c: "squash" });
-  expect(2 + 2).toBe(4);
   expect(res.status).toBe(200);
+});
+
+test("read query", async () => {
+  const rows = await read01({});
+  expect(
+    JSON.stringify(rows) ===
+      JSON.stringify([{ c: "carrot" }, { c: "tomato" }, { c: "squash" }]),
+  );
 });
