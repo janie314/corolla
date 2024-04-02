812346b1cd00b3561d98238c00180ae7d1131baa
12ball <pub@janie.page>
2024-04-01 22:21:56 -0500

    1712028116

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,83 +3,14 @@ use pretty_assertions::assert_eq;
 use reqwest::StatusCode;
 use std::{
     collections::HashMap,
-    path::{Path, PathBuf},
     process::{Command, Stdio},
     thread,
 };
 
-fn cleanup(path: &str, pid: Option<u32>) {
-    for file in [
-        "corolla-integration-test.sqlite3",
-        "corolla-integration-test.sqlite3-shm",
-        "corolla-integration-test.sqlite3-wal",
-    ]
-    .iter()
-    {
-        Command::new("rm")
-            .arg(Path::new(path).join(file).to_str().unwrap())
-            .output()
-            .expect("could not execute cleanup step");
-    }
-    match pid {
-        Some(pid) => {
-            Command::new("kill")
-                .arg(pid.to_string())
-                .output()
-                .expect("could not kill corolla; this will require manual cleanup");
-        }
-        None => (),
-    }
-}
-
-fn get_root_dir() -> PathBuf {
-    Path::new(env!("CARGO_BIN_EXE_corolla"))
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .to_path_buf()
-}
-
-fn start_server(spec_path: &Path) {
-    let root_dir = get_root_dir();
-    let corolla = Command::new(env!("CARGO_BIN_EXE_corolla"))
-        .args([
-            "-s",
-            spec_path.to_str().unwrap(),
-            "-d",
-            Path::new(&root_dir)
-                .join("tmp")
-                .join("corolla-integration-test.sqlite3")
-                .to_str()
-                .unwrap(),
-        ])
-        .stderr(Stdio::null())
-        .stdout(Stdio::null())
-        .spawn()
-        .expect("failed to run corolla with examples/example_spec.json");
-}
-
 #[test]
 fn integration_test() {
-    cleanup(path, None);
-    let corolla = Command::new(env!("CARGO_BIN_EXE_corolla"))
-        .args([
-            "-s",
-            "examples/example_spec.json",
-            "-d",
-            Path::new(path)
-                .join("tmp")
-                .join("corolla-integration-test.sqlite3")
-                .to_str()
-                .unwrap(),
-        ])
-        .stderr(Stdio::null())
-        .stdout(Stdio::null())
-        .spawn()
-        .expect("failed to run corolla with examples/example_spec.json");
+    cleanup(None);
+    let corolla = server("examples/example_spec.json");
     thread::sleep(time::Duration::from_secs(10));
     let inputs = ["1-2-3", "do-re-mi", "baby you and me"];
     let client = reqwest::blocking::Client::new();
@@ -116,12 +47,7 @@ fn integration_test() {
         .arg(corolla.id().to_string())
         .output()
         .expect("could not kill corolla; this will require manual cleanup");
-    let corolla = Command::new(env!("CARGO_BIN_EXE_corolla"))
-        .args(["-s", "examples/example_spec_with_conversions.json"])
-        .stderr(Stdio::null())
-        .stdout(Stdio::null())
-        .spawn()
-        .expect("failed to run corolla with examples/example_spec.json");
+    let corolla = server("examples/example_spec_with_conversions.json");
     thread::sleep(time::Duration::from_secs(2));
     let inputs = [
         ("cargo test 90210", "what we have here"),
@@ -162,5 +88,5 @@ fn integration_test() {
         assert_eq!(row.get(0).unwrap(), x);
         assert_eq!(row.get(1).unwrap(), y);
     }
-    cleanup(path, Some(corolla.id()));
+    cleanup(Some(corolla.id()));
 }
