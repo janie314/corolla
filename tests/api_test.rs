a9e6026cf48dc4c81ca4d3f26c1951e30ac5d6be
12ball <pub@janie.page>
2024-04-08 07:51:56 -0500

    1712580716

--- tests/api_test.rs
+++ tests/api_test.rs
@@ -1,13 +1,15 @@
 use crate::common::cleanup;
 use common::server;
-use std::process::Command;
+use std::{env::var, process::Command};
 
 mod common;
 
 #[tokio::test]
 async fn api_test() {
     let mut corolla = server("examples/example_spec.json").await;
-    let mut bun_test = Command::new("bun")
+    let bun = var("BUN_RUNTIME").expect("invalid environment variable BUN_RUNTIME");
+    assert_ne!(bun.len(), 0);
+    let mut bun_test = Command::new(bun)
         .args(["test"])
         .spawn()
         .expect("failed to run `bun test`");
